53431762bb60ac2fe7dd3ec43e10118dfff1ea8b
Junaed <junaed1995@gmail.com>
2021-11-03 19:38:59 +0600

    * Ui with Container and Row

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const CircleAvatar(
                 radius: 50.0,
@@ -37,6 +38,56 @@ class MyApp extends StatelessWidget {
                   fontSize: 20,
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: const EdgeInsets.only(
+                    top: 30, bottom: 10, left: 25, right: 25),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+88 01723605770",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source_Sans_Pro",
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "junaed1995@gmail.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source_Sans_Pro",
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
